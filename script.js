ad07a5827fca874df86f404eb18dc1dca642f443
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-17 15:31:29 +0200

    first upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// ex 1
+// const app =  Vue.createApp({
+// template:'<h2>the vue is here</h2>'
+// });
+
+// app.mount('#app');
+
+// ex2
+// const app =  Vue.createApp({
+//     data(){
+//         return{
+//             title:"On my way to becoming dope",
+//             description:"im a goat and i am",
+//             age:27
+//         }
+//     }
+    
+//     });
+    
+//     app.mount('#app');
+
+// exe3
+    // const app =  Vue.createApp({
+    //     data(){
+    //         return{
+    //             title:"On my way to becoming dope",
+    //             description:"im a goat and i am",
+    //             age:27
+    //         }
+    //     }
+        
+    //     });
+        
+    //     app.mount('#app')
+
+// exe 4
+        const app =  Vue.createApp({
+            data(){
+                return{
+                    title:"On my way to becoming dope",
+                    description:"im a goat and i am",
+                    age:27
+                }
+            },
+            methods: {
+                
+            }
+            
+            });
+            
+            app.mount('#app')
