2c85e31e443ab32f690d484ef779cc005b739a15
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-18 09:39:29 +0200

    update

--- script.js
+++ script.js
@@ -6,13 +6,26 @@
                     title:"John",
                     description:"im a goat and ",
                     sentence:"i am",
-                    age:27
+                    age:27,
+                    x:0, 
+                    y:0
                 }
             },
             methods: {
            toggleShowSentence(){
                this.showSentence =!this.showSentence
+           },
+           handleEvent(e,number){
+            console.log(e,e.type);
+            if(number){
+                console.log(number)
+            }
+           },
+           handleMouseMove(e){
+               this.x=e.offsetX;
+               this.y = e.offsetY;
            }
+       
             }
             
             })
