2f00f2d607d7a0fdb58fb00b4429b8cc4791ace6
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-18 09:14:18 +0200

    added branches

--- script.js
+++ script.js
@@ -1,51 +1,20 @@
-// ex 1
-// const app =  Vue.createApp({
-// template:'<h2>the vue is here</h2>'
-// });
 
-// app.mount('#app');
-
-// ex2
-// const app =  Vue.createApp({
-//     data(){
-//         return{
-//             title:"On my way to becoming dope",
-//             description:"im a goat and i am",
-//             age:27
-//         }
-//     }
-    
-//     });
-    
-//     app.mount('#app');
-
-// exe3
-    // const app =  Vue.createApp({
-    //     data(){
-    //         return{
-    //             title:"On my way to becoming dope",
-    //             description:"im a goat and i am",
-    //             age:27
-    //         }
-    //     }
-        
-    //     });
-        
-    //     app.mount('#app')
-
-// exe 4
         const app =  Vue.createApp({
             data(){
                 return{
-                    title:"On my way to becoming dope",
-                    description:"im a goat and i am",
+                    showSentence: true,
+                    title:"John",
+                    description:"im a goat and ",
+                    sentence:"i am",
                     age:27
                 }
             },
             methods: {
-                
+           toggleShowSentence(){
+               this.showSentence =!this.showSentence
+           }
             }
             
-            });
+            })
             
-            app.mount('#app')
+            app.mount('#app');
